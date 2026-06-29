59d402f6e8c2fa197625fc32b79ed11a4d90210d
jiftuq <cizac2000@gmail.com>
2026-06-29 09:33:40 +0200

    continue

---
+++ stagiaires/Christophe/public/index.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * CONTROLEUR FRONTAL
+ */
+
+//lancement d'une session (PHPSESSID)
+session_start();
+
+//chargement des dépendances
+require_once '../config.php';
+
+//chargement du routeur
+require_once '../router.php';
