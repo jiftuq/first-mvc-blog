7e0b6b846b75544a23b2bd162be47c3c0c4e20e3
WebDevCF2m2026 <gitweb@cf2m.be>
2026-06-29 10:04:40 +0200

    exe1

--- formateur/model/ArticleModel.php
+++ formateur/model/ArticleModel.php
@@ -13,6 +13,8 @@
 
 function selectAllArticleHomepage(PDO $db): array 
 {
+    # on sélectionne id, title, date et 250 caractères de content de la table article ordonné par date DESC. On prend ensuite en jointure interne id renommé iduser et username de la table user.
+    # exercice Je veux récupérer id renommé idcateg (groupé avec la , comme séparateur) et title renommé titlecateg (groupé avec les '|||' comme séparateur) de la table category (jointure externe non obligatoire ! m2m ! (seuls les articles doivent être présent), il faut regrouper les articles pour n'en avoir qu'un article par page
     $sql = "SELECT 
 	a.`id`, a.`title`,a.`date`, LEFT(a.`content`,250) AS `content` ,
     u.`id` AS `iduser`, u.`username`
